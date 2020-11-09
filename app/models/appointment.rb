77c2754c2ffcb6b98e00e765723ae2b9b4a23575
Alex Rios <alexrios.dev@gmail.com>
2020-11-09 13:02:10 -0600

    Associations done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,4 @@
 class Appointment < ApplicationRecord
+  belongs_to :employee
+  belongs_to :client
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ApplicationRecord
+  has_many :appointments
+  has_many :clients, through: :appointments
+
+  belongs_to :department
 end
