378c6d8e65397d8af9c199bced523d39fa3e6bb1
Alex Rios <alexrios.dev@gmail.com>
2020-11-09 15:05:43 -0600

    Added views.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -8,4 +8,23 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.find_by(id: params[:id])
   end
 
+  def new
+    @appointment = Appointment.new
+    @employees = Employee.all
+    @clients = Client.all
+  end
+
+  def create
+    @appointment = Appointment.new(strong_params)
+    @appointment.admin_id = Admin.first
+    @appointment.save
+    redirect_to admin_path(Admin.first)
+  end
+
+  private
+
+  def strong_params
+    params.require(:appointment).permit(:appointment_time, :purpose, :message, :employee_id, :client_id)
+  end
+
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :employee
   belongs_to :client
   belongs_to :admin
+
+  def time_select
+  end
+
 end
