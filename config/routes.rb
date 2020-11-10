c9eeffe7e9a87e26f47cef9a0cc92678bd64ea77
sannydo <sanny.do94@gmail.com>
2020-11-10 16:04:05 -0600

    sanny commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :admins
   resources :clients
   resources :appointments
   resources :employees
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "admins#index"
   
 end
