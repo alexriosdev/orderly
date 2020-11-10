81b514f740229c0265028b07726bbf43cc0e9a58
sannydo <sanny.do94@gmail.com>
2020-11-09 22:32:43 -0600

    sannys first commit

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,27 @@
 class AdminsController < ApplicationController
 
+  def index
+    @admins = Admin.all
+  end
+
   def show
     @admin = Admin.find_by(id: params[:id])
   end
+
+  # def new
+  #   @user.User.new
+  # end
+
+  # def create
+  #   @user = User.new(params[:user])
+  #   if @user.save
+  #     flash[:notice] = "Welcome new user!"
+  #     flash[:color]= "valid"
+  #   else
+  #     flash[:notice] = "Invalid, please try again."
+  #     flash[:color] = "invalid"
+  #   end
+  #   render "new"
+  # end 
 
 end
