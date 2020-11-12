18eb8f7b03f3311321a6e2c4b5e775d325adee94
sannydo <sanny.do94@gmail.com>
2020-11-12 16:58:03 -0600

    sign out complete

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,10 @@ Rails.application.routes.draw do
   
   # working
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root to: 'appointments#index'
-  resources :users
   resources :clients
   resources :appointments
 
